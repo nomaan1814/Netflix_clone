918906e5e25b5f763315154ccbf33a7dc6eeb101
nomaan1814 <mohdnomaan181149@gmail.com>
2022-03-15 19:20:19 +0530

    Banner update

--- src/Banner.js
+++ src/Banner.js
@@ -36,8 +36,10 @@ function Banner(){
                         <button  className="banner__button">Play</button>
                         <button  className="banner__button">My List</button>
                   </div>  
-                  <h1 className="banner__description">{movie?.overview}</h1>
-              </div>  
+                  <h1 className="banner__description">{truncate(movie?.overview,150)}</h1>
+              </div> 
+              <div className="banner--fadeBottom">
+              </div> 
           </header>
         </>)
 }
